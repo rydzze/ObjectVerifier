308470d8112de7f31d6ffd649728796031699f0e
Rydzze <rydzze@gmail.com>
2024-05-29 00:02:57 +0800

    adjust dataset

--- img2h5.py
+++ img2h5.py
@@ -4,10 +4,14 @@ import h5py
 import numpy as np
 
 FOLDER_PATH = ["Object 1", "Object 2", "Object 3", "Object 4"]
-NEW_WIDTH = 1500
-NEW_HEIGHT = 1000
+NEW_WIDTH = 750
+NEW_HEIGHT = 500
 
 with h5py.File('dataset.h5', 'w') as hdf:
+    color_images = []
+    grey_images = []
+    labels = []
+    
     for obj, path in enumerate(FOLDER_PATH, start=1):
         images = [file for file in os.listdir(path) if file.endswith(('.JPG', '.jpg'))]
         
@@ -18,14 +22,14 @@ with h5py.File('dataset.h5', 'w') as hdf:
             color_img = cv2.resize(image, (NEW_WIDTH, NEW_HEIGHT))
             grey_img = cv2.cvtColor(color_img, cv2.COLOR_BGR2GRAY)
             
-            color_img_array = np.array(color_img)
-            grey_img_array = np.array(grey_img)
-            color_dataset_name = f'Object{obj}_Color_{i+1:02d}'
-            grey_dataset_name = f'Object{obj}_Greyscale_{i+1:02d}'
-            
-            hdf.create_dataset(color_dataset_name, data=color_img_array, compression="gzip", compression_opts=9)
-            hdf.create_dataset(grey_dataset_name, data=grey_img_array, compression="gzip", compression_opts=9)
-
+            color_images.append(color_img)
+            grey_images.append(grey_img)
+            labels.append(obj)
+        
         print(f"Object {obj} done ...")
+        
+    hdf.create_dataset('Objects_Color', data=np.array(color_images), compression="gzip", compression_opts=9)
+    hdf.create_dataset('Objects_Greyscale', data=np.array(grey_images), compression="gzip", compression_opts=9)
+    hdf.create_dataset('Labels', data=np.array(labels), compression="gzip", compression_opts=9)
 
 print("All images have been stored in the HDF5 dataset.")
