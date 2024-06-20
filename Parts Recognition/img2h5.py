1952a543a241ff81ff555ec9cbb519d38434cbdb
Rydzze <rydzze@gmail.com>
2024-06-21 03:38:53 +0800

    finalise source code

--- Parts Recognition/img2h5.py
+++ Parts Recognition/img2h5.py
@@ -3,29 +3,39 @@ import cv2
 import h5py
 import numpy as np
 
+# Define the constant variables
 FOLDER_PATH = ["Object 1", "Object 2", "Object 3", "Object 4"]
 NEW_WIDTH = 750
 NEW_HEIGHT = 500
 
+# Create a new HDF5 file for storing data
 with h5py.File('dataset.h5', 'w') as hdf:
-    color_images = []
+    images = []
     labels = []
     
+    # Loop through each folder
     for obj, path in enumerate(FOLDER_PATH, start=1):
-        images = [file for file in os.listdir(path) if file.lower().endswith('.jpg')]
+        # Retrieve all JPEG files in the folder
+        files = [file for file in os.listdir(path) if file.lower().endswith('.jpg')]
         
-        for i, img in enumerate(sorted(images)):
+        print(f"\nProcessing the images of Object {obj} ...")
+
+        # Process each image in the folder
+        for i, img in enumerate(sorted(files)):
             img_path = os.path.join(path, img)
             
+            # Read and then resize the image
             image = cv2.imread(img_path)
-            color_img = cv2.resize(image, (NEW_WIDTH, NEW_HEIGHT))
+            image = cv2.resize(image, (NEW_WIDTH, NEW_HEIGHT))
             
-            color_images.append(color_img)
+            # Append the image and label in list
+            images.append(image)
             labels.append(obj)
         
-        print(f"Object {obj} done ...")
-        
-    hdf.create_dataset('Images', data=np.array(color_images), compression="gzip", compression_opts=9)
+        print(f"Images of Object {obj} has been processed ...")
+    
+    # Convert lists into numpy arrays and store in HDF5
+    hdf.create_dataset('Images', data=np.array(images), compression="gzip", compression_opts=9)
     hdf.create_dataset('Labels', data=np.array(labels), compression="gzip", compression_opts=9)
 
-print("All images have been stored in the HDF5 dataset.")
+print("\nAll images and labels have been stored in the HDF5 dataset.")
--- Parts Recognition/rename.py
+++ Parts Recognition/rename.py
@@ -1,50 +1,54 @@
 import os
 import shutil
 
-FOLDER_PATHS = {
-    "Object 1": {
-        "0_whiteBg": 37,
-        "1_greenBg": 14,
-        "2_blackBg": 12
-    },
-    "Object 2": {
-        "0_whiteBg": 31,
-        "1_greenBg": 16,
-        "2_blackBg": 12
-    },
-    "Object 3": {
-        "0_whiteBg": 38,
-        "1_greenBg": 17,
-        "2_blackBg": 13
-    },
-    "Object 4": {
-        "0_whiteBg": 72,
-        "1_greenBg": 17,
-        "2_blackBg": 10
-    }
-}
+# Define a dict with folder paths and respective image counts
+FOLDER_PATHS = {"Object 1": {"1_whiteBg": 37,
+                             "1_greenBg": 14,
+                             "1_blackBg": 12},
+                "Object 2": {"2_whiteBg": 31,
+                             "2_greenBg": 16,
+                             "2_blackBg": 12},
+                "Object 3": {"3_whiteBg": 38,
+                             "3_greenBg": 17,
+                             "3_blackBg": 13},
+                "Object 4": {"4_whiteBg": 72,
+                             "4_greenBg": 17,
+                             "4_blackBg": 10}}
 
+# Function to rename image files in a folder according to specified counts
 def rename_images(folder_path, counts):
-    counters = {img_type: 1 for img_type in counts.keys()}
+    # Initialise counters for each image type
+    count = {img_type: 1 for img_type in counts.keys()}
     
+    # Retrieve all JPEG files in the folder
     files = sorted([f for f in os.listdir(folder_path) if f.lower().endswith('.jpg')])
     
-    for img_type, count in counts.items():
+    # Process each image 
+    for img, count in counts.items():
+        # Rename each image according to the specified count
         for old_name in files[:count]:
             _, ext = os.path.splitext(old_name)
-            new_name = f"{img_type}_{counters[img_type]:02d}{ext.lower()}"
+            new_name = f"{img}_{count[img]:02d}{ext.lower()}"
             old_path = os.path.join(folder_path, old_name)
             new_path = os.path.join(folder_path, new_name)
             
+            # Execute the file renaming process
+            print(f"Renaming {old_path} to {new_path}")
             shutil.move(old_path, new_path)
-            print(f"Renamed {old_path} to {new_path}")
             
-            counters[img_type] += 1
+            # Increase counter
+            count[img] += 1
         
+        # Remove processed files from the list
         files = files[count:]
 
+# Iterate over each object and its corresponding image counts
 for obj, counts in FOLDER_PATHS.items():
+    # Construct the full folder path
     folder_path = os.path.join(os.getcwd(), obj)
+
+    # Call the rename_images function for each object
     rename_images(folder_path, counts)
 
-print("Image renaming completed.")
+# Print complete message
+print("Image renaming process completed :D")
